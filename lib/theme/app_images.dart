286dd8e2fcde2b636fc35aed5152b0b1dacb3032
catalunha <catalunha.mj@gmail.com>
2021-11-09 18:38:03 -0300

    limpando e clonado de dictation

---
+++ lib/theme/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const branches = "assets/images/branches.png";
+  static const three = "assets/images/three.png";
+  static const tree = "assets/images/tree.png";
+  static const phrase = "assets/images/phrase.png";
+}
