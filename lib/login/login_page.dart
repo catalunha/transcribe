6540d6db29e4cfa92d936ab1a800df29c293abdc
catalunha <catalunha.mj@gmail.com>
2021-11-18 21:19:21 -0300

    icones

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -14,51 +14,41 @@ class LoginPage extends StatelessWidget {
 
     return Scaffold(
       // backgroundColor: AppColors.background,
-      body: SizedBox(
-        width: size.width,
-        height: size.height,
-        child: Stack(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              width: size.width,
-              height: size.height * 0.4,
-              color: Colors.green,
+            // const SizedBox(height: 100),
+            Image.asset(
+              AppImages.login,
+              // width: 192,
+              // height: 192,
             ),
-            Center(
-              child: Column(
-                children: [
-                  const SizedBox(height: 50),
-                  Image.asset(
-                    AppImages.waveLine,
-                    width: 208,
-                    height: 273,
-                  ),
-                  const SizedBox(height: 10),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 20, right: 20),
-                    child: Text(
-                      'Welcome to Transcribing.',
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  // GoogleLoginButton(
-                  //   onTap: signIn,
-                  // ),
-                  OutlinedButton.icon(
-                    onPressed: signIn,
-                    icon: Image.asset(AppImages.google),
-                    label: const Text(
-                      'Select your GOOGLE account',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    style: OutlinedButton.styleFrom(
-                        padding: const EdgeInsets.all(20)),
-                  ),
-                ],
+            const SizedBox(height: 50),
+            const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Text(
+                'Welcome to Transcribing.',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
               ),
-            )
+            ),
+            const SizedBox(height: 30),
+            // GoogleLoginButton(
+            //   onTap: signIn,
+            // ),
+            OutlinedButton.icon(
+              onPressed: signIn,
+              icon: Image.asset(AppImages.google),
+              label: const Text(
+                'Select your GOOGLE account',
+                style: TextStyle(fontSize: 16),
+              ),
+              style:
+                  OutlinedButton.styleFrom(padding: const EdgeInsets.all(20)),
+            ),
           ],
         ),
       ),
--- lib/theme/app_images.dart
+++ lib/theme/app_images.dart
@@ -1,8 +1,4 @@
 class AppImages {
-  static const branches = "assets/images/branches.png";
-  static const three = "assets/images/three.png";
-  static const tree = "assets/images/tree.png";
-  static const phrase = "assets/images/phrase.png";
-  static const waveLine = "assets/images/waveLine.png";
+  static const login = "assets/images/login.png";
   static const google = "assets/images/google.png";
 }
